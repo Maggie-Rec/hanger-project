e34a2e2d1f085acd9732eb0ca772bd0a41693adb
Maggie Recio <m.reciogrom@gmail.com>
2023-06-15 13:47:05 +0100

    server

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 main().catch((err) => console.log(err));
 
 async function main() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/HangerDB');
-    console.log('connected to mongoDB');
+    await mongoose.connect(
+      "mongodb+srv://test_user:N3MZjiiY1ZcKLFxc@cluster0.9xe7zsw.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("connected to mongoDB");
   } catch (error) {
     console.error(`Error connecting to MongoDB:`, error);
   }
