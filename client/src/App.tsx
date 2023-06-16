446a7d31965f2c014b80d84dceea809b84b5e92f
Maggie Recio <m.reciogrom@gmail.com>
2023-06-16 12:04:13 +0100

    added theme provider from tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,55 +1,58 @@
-import './App.css';
-import React, { useState } from 'react';
-import MainPage from './components/MainPage';
-import Profile from './components/Profile';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginView from './components/LoginView';
+import "./App.css";
+import { ThemeProvider } from "@material-tailwind/react";
+import React, { useState } from "react";
+import MainPage from "./components/MainPage";
+import Profile from "./components/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginView from "./components/LoginView";
 // In your root application component (e.g., App.tsx)
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import PaymentForm from './components/PaymentForm';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import PaymentForm from "./components/PaymentForm";
 
 // Wrap your components with Stripe's Elements provider
 
 const App: React.FC = () => {
   const [component, setComponent] = useState(true);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const stripePromise = loadStripe(
-    'pk_test_51N30rHGF7NYNyMJj7sD45qKrarWZcDRJVaxKsiK4S6kUTgUeGsP8cszeVpftpMrhQi3BJQz10JEsZZjMQakx9eDp00SEEjtMUN'
+    "pk_test_51N30rHGF7NYNyMJj7sD45qKrarWZcDRJVaxKsiK4S6kUTgUeGsP8cszeVpftpMrhQi3BJQz10JEsZZjMQakx9eDp00SEEjtMUN"
   );
 
   const toggleComponent = () => {
     setComponent(!component);
   };
   return (
-    <Elements stripe={stripePromise}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/payment' element={<PaymentForm />} />
-          <Route path='/login' element={<LoginView />} />
-          <Route
-            path='/'
-            element={
-              <>
-                {component ? (
-                  <MainPage
-                    toggleComponent={toggleComponent}
-                    searchText={searchText}
-                    setSearchText={setSearchText}
-                  />
-                ) : (
-                  <Profile
-                    toggleComponent={toggleComponent}
-                    setSearchText={setSearchText}
-                  />
-                )}
-                ;
-              </>
-            }
-          ></Route>
-        </Routes>
-      </BrowserRouter>
-    </Elements>
+    <ThemeProvider>
+      <Elements stripe={stripePromise}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/payment" element={<PaymentForm />} />
+            <Route path="/login" element={<LoginView />} />
+            <Route
+              path="/"
+              element={
+                <>
+                  {component ? (
+                    <MainPage
+                      toggleComponent={toggleComponent}
+                      searchText={searchText}
+                      setSearchText={setSearchText}
+                    />
+                  ) : (
+                    <Profile
+                      toggleComponent={toggleComponent}
+                      setSearchText={setSearchText}
+                    />
+                  )}
+                  ;
+                </>
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </Elements>
+    </ThemeProvider>
   );
 };
 
